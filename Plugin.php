8a01480a097b497b3cdbbb8f0d52bfc72c253480
Jesse Visser <hallo@jnvisser.nl>
2025-09-26 22:00:19 +0200

    Remove configure action

--- Plugin.php
+++ Plugin.php
@@ -3,10 +3,7 @@
 namespace App\Vito\Plugins\Flowan\VitoServiceSonarr;
 
 use App\Plugins\AbstractPlugin;
-use App\Plugins\RegisterServerFeature;
-use App\Plugins\RegisterServerFeatureAction;
 use App\Plugins\RegisterServiceType;
-use App\Vito\Plugins\Flowan\VitoServiceSonarr\Actions\Configure;
 use App\Vito\Plugins\Flowan\VitoServiceSonarr\Services\Sonarr;
 
 class Plugin extends AbstractPlugin
@@ -24,16 +21,6 @@ class Plugin extends AbstractPlugin
             ->versions([
                 'latest',
             ])
-            ->register();
-
-        RegisterServerFeature::make('sonarr')
-            ->label($this->name)
-            ->description('Configure Sonarr on your server.')
-            ->register();
-
-        RegisterServerFeatureAction::make('sonarr', 'configure')
-            ->label('Configure')
-            ->handler(Configure::class)
             ->register();
     }
 }
